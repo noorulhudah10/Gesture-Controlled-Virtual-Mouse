0ef1b2b10cc5940a344ff6a32fd0058a11ad8119
noorulhudah10 <102608140+noorulhudah10@users.noreply.github.com>
2023-10-16 09:54:20 -0700

    Update Gesture_Controller.py

--- src/Gesture_Controller.py
+++ src/Gesture_Controller.py
@@ -7,7 +7,7 @@ import math
 from enum import IntEnum
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pycaw.pycaw import IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
 import screen_brightness_control as sbcontrol
 
